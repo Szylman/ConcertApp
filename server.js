1f0c9bcb9343b4ab1e601daba8fb6111ace6f2a0
Szylman <szymek2255@gmail.com>
2022-11-04 16:19:51 +0100

    add helmet and mongo-sanitize to project

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cors = require('cors');
 const socket = require('socket.io');
 const mongoose = require('mongoose');
+const helmet = require('helmet');
 
 const server = app.listen(process.env.PORT || 8000, () => {
     console.log('Server is running on port: 8000');
@@ -12,6 +13,7 @@ const server = app.listen(process.env.PORT || 8000, () => {
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
+app.use(helmet());
 
 // connects our backend code with the database
 mongoose.connect('mongodb+srv://Szylman:qwerty123@cluster0.iarn6tt.mongodb.net/ConcertDB',
